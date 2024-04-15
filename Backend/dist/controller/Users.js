85381ae91033f10e2bf9cbb84765a9ae1d1a19e6
kirengamartial <kirengamartial@gmail.com>
2024-04-15 23:44:02 +0200

    new changes

--- Backend/dist/controller/Users.js
+++ Backend/dist/controller/Users.js
@@ -219,7 +219,7 @@ export const loginUser = async (req, res) => {
             const auth = await bcrypt.compare(password, user.password);
             if (auth) {
                 const token = cookieToken(user._id);
-                res.cookie('jwt', token, { maxAge: 3 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true, path: '/' });
+                res.cookie('jwt', token, { maxAge: 3 * 24 * 60 * 60 * 1000, httpOnly: true, path: '/' });
                 res.status(200).json({ user: user._id });
             }
             else {
--- Backend/dist/src/index.js
+++ Backend/dist/src/index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://kirengamartial.github.io',
+    origin: '*',
     credentials: true
 }));
 const options = {
