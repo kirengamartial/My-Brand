c5cec7d264ff2ae2b4f5a5c44d8448f8870f8068
kirengamartial <kirengamartial@gmail.com>
2024-04-15 21:25:03 +0200

    new changes

--- Backend/dist/src/index.js
+++ Backend/dist/src/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://kirengamartial.github.io/',
+    origin: 'https://kirengamartial.github.io',
     credentials: true
 }));
 const options = {
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -21,7 +21,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: 'https://kirengamartial.github.io/',
+  origin: 'https://kirengamartial.github.io',
   credentials: true
 }))
 
