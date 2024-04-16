cb271054f716d7c42fd69a34ddae5e719a286ada
kirengamartial <kirengamartial@gmail.com>
2024-04-16 17:25:42 +0200

    new changes'

--- Frontend/assets/js/index.js
+++ Frontend/assets/js/index.js
@@ -58,8 +58,10 @@ document.addEventListener('DOMContentLoaded', () => {
         document.addEventListener('click', (e) => __awaiter(void 0, void 0, void 0, function* () {
             if (e.target instanceof HTMLElement && e.target.id === 'logout') {
                 e.preventDefault();
-                document.cookie = `jwt=''; max-age=1;; path=/;`;
-                window.location.href = 'register.html';
+                document.cookie = `jwt=''; max-age=1; path=/;`;
+                setTimeout(() => {
+                    window.location.href = 'register.html';
+                }, 1000); // Redirect after 1 second
             }
         }));
         const cookie = document.cookie.split('jwt=')[1];
