4aea0dfb93e0282eb99aa029dd37fde55a18f5ed
kirengamartial <kirengamartial@gmail.com>
2024-04-21 18:51:25 +0200

    new changes

--- src/ContentBlog1.ts
+++ src/ContentBlog1.ts
@@ -135,6 +135,7 @@ fetch('https://my-brand-backend-h88y.onrender.com/api/user', {
                         `;
                     }
                 } else {
+                    
                    popup.innerHTML = `
                    <div class="popup-container">
                    <div id="popup success" class="popup">
