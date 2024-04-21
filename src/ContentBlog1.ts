eeb830200bb9e607117a5b54e500ecb0bb023a1b
kirengamartial <kirengamartial@gmail.com>
2024-04-21 18:49:48 +0200

    new changes

--- src/ContentBlog1.ts
+++ src/ContentBlog1.ts
@@ -142,7 +142,6 @@ fetch('https://my-brand-backend-h88y.onrender.com/api/user', {
                    </div>
                    `
                 }
-                
             } catch (error) {
                 console.error('Error submitting comment:', error);
             }
